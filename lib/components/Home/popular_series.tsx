5f839a9cadcf67aa8303b593cb3c498b9e5e8bca
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-05-24 12:54:56 +0000

    fix: loading state

--- lib/components/Home/popular_series.tsx
+++ lib/components/Home/popular_series.tsx
@@ -25,38 +25,42 @@ export function HomePopularSeries() {
         </span>
       </div>
       <div className="mt-4 grid grid-cols-2 gap-y-5 sm:grid-cols-3 gap-x-5">
-        {data.popular.results.slice(0, 5).map((serie) => (
-          <Link href={`/dizi/${slugify(serie.original_name)}`} key={serie.id}>
-            <div
-              className={classNames(
-                "flex flex-col rounded-xl overflow-hidden cursor-pointer group select-none relative",
-                serie.name && "border dark:border-zinc-600"
-              )}
-            >
-              <div className="absolute inset-0 bg-black/60 none flex-col items-center justify-center gap-4 text-sm font-bold opacity-0 group-hover:opacity-100 duration-300 text-center hidden md:flex  ">
-                <div className="self-center flex flex-col items-center space-y-2">
-                  <span className="capitalize text-white font-medium drop-shadow-md">
-                    {serie.original_name}
-                  </span>
+        {isPending ? (
+          <Loading />
+        ) : (
+          data.airToday.results.slice(0, 5).map((serie) => (
+            <Link href={`/dizi/${slugify(serie.original_name)}`} key={serie.id}>
+              <div
+                className={classNames(
+                  "flex flex-col rounded-xl overflow-hidden cursor-pointer group select-none relative",
+                  serie.name && "border dark:border-zinc-600"
+                )}
+              >
+                <div className="absolute inset-0 bg-black/60 none flex-col items-center justify-center gap-4 text-sm font-bold opacity-0 group-hover:opacity-100 duration-300 text-center hidden md:flex  ">
+                  <div className="self-center flex flex-col items-center space-y-2">
+                    <span className="capitalize text-white font-medium drop-shadow-md">
+                      {serie.original_name}
+                    </span>
+                  </div>
                 </div>
+
+                <img
+                  src={tmdbPoster(serie.poster_path ?? "")}
+                  className="h-full w-full"
+                  alt={serie.original_name}
+                />
+
+                {serie.original_name && (
+                  <div className="w-full bg-white dark:bg-zinc-800 dark:text-white px-3 flex py-5 items-center justify-between border-t-2 border-t-red-600">
+                    <span className="capitalize  font-medium truncate">
+                      {serie.original_name}
+                    </span>
+                  </div>
+                )}
               </div>
-
-              <img
-                src={tmdbPoster(serie.poster_path ?? "")}
-                className="h-full w-full"
-                alt={serie.original_name}
-              />
-
-              {serie.original_name && (
-                <div className="w-full bg-white dark:bg-zinc-800 dark:text-white px-3 flex py-5 items-center justify-between border-t-2 border-t-red-600">
-                  <span className="capitalize  font-medium truncate">
-                    {serie.original_name}
-                  </span>
-                </div>
-              )}
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))
+        )}
       </div>
     </section>
   );
