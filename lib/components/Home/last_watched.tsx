29ec04732733a366eea2461f9790977265d6d66b
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-06-23 18:44:57 +0200

    fix: center loading

--- lib/components/Home/last_watched.tsx
+++ lib/components/Home/last_watched.tsx
@@ -4,7 +4,6 @@ import { slugify } from "@/lib/helpers";
 import UserService from "@/lib/services/user.service";
 import { useAuthStore } from "@/lib/stores/auth.store";
 import { useQuery } from "@tanstack/react-query";
-import { Suspense } from "react";
 import { Loading } from "../Loading";
 import { SerieCard } from "./SerieCard";
 
@@ -21,7 +20,12 @@ export function HomeLastWatched() {
 
   if (isError) return <div>Error</div>;
 
-  if (isPending) return <Loading />;
+  if (isPending)
+    return (
+      <div className="flex justify-center items-center">
+        <Loading />
+      </div>
+    );
 
   if (data && data.length < 1) return null;
 
@@ -34,30 +38,28 @@ export function HomeLastWatched() {
           </span>
         </div>
         <div className="mt-4 grid grid-cols-2 gap-y-5 sm:grid-cols-3 gap-x-5">
-          <Suspense fallback={<Loading />}>
-            {data?.slice(0, 5).map((serie) => {
-              let href = `/dizi/${slugify(serie.name)}/sezon-${
-                serie.season
-              }/bolum-${serie.episode}`;
+          {data?.slice(0, 5).map((serie) => {
+            let href = `/dizi/${slugify(serie.name)}/sezon-${
+              serie.season
+            }/bolum-${serie.episode}`;
 
-              if (serie.network) {
-                href += `?network=${serie.network}`;
-              }
+            if (serie.network) {
+              href += `?network=${serie.network}`;
+            }
 
-              const name = `${serie.name} - S:${serie.season} E:${serie.episode}`;
-              return (
-                <SerieCard
-                  key={serie.id}
-                  serie={{
-                    name: name,
-                    tmdb_id: serie.tmdb_id,
-                    image: serie.poster_path,
-                    href: href,
-                  }}
-                />
-              );
-            })}
-          </Suspense>
+            const name = `${serie.name} - S:${serie.season} E:${serie.episode}`;
+            return (
+              <SerieCard
+                key={serie.id}
+                serie={{
+                  name: name,
+                  tmdb_id: serie.tmdb_id,
+                  image: serie.poster_path,
+                  href: href,
+                }}
+              />
+            );
+          })}
         </div>
       </section>
     </div>
