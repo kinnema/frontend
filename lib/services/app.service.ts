a531682e8eed3b6430d2a5e6b5e41523de8bfb3e
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2025-01-15 00:39:47 +0100

    fix: https

--- lib/services/app.service.ts
+++ lib/services/app.service.ts
@@ -29,7 +29,7 @@ export default class AppService {
     search_params.append("season", season.toString());
     search_params.append("episode", chapter.toString());
 
-    const response = await appAxiosClient.get(`/watch/?${search_params}`);
+    const response = await appAxiosClient.get(`/watch?${search_params}`);
     return JSON.parse(response.data);
   };
 }
