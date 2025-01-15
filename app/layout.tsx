7d042826a979df219c55e0831eb56b9eda6669fa
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2025-01-15 06:06:14 +0000

    fix: force dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   modal: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
