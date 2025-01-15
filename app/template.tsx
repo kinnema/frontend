567cc051193ddc32d9ce4cd8390f7ebcdb38bc07
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2025-01-15 06:37:09 +0000

    fix: remove transition

--- app/template.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Transition } from "@/lib/components/Transition";
-import { PropsWithChildren } from "react";
-
-export default function Template({ children }: PropsWithChildren) {
-  return <Transition>{children}</Transition>;
-}
