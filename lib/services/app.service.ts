4447a31fb073cf9c7dc1ad18d0111926fca3d6c2
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2025-06-20 20:26:14 +0200

    refactor: remove unused watch event types and clean up app service

--- lib/services/app.service.ts
+++ lib/services/app.service.ts
@@ -1,15 +1,5 @@
-import { EventEmitter } from "events";
-import type TypedEmitter from "typed-emitter";
 import { Configuration, DefaultApi, RequestContext } from "../api";
 import { BASE_URL } from "../constants";
-import {
-  IWatchEvent,
-  IWatchEventEmitter,
-  IWatchEventInit,
-  IWatchEventProviderFailed,
-  IWatchEventProviderSuccess,
-  IWatchEventTryingProvider,
-} from "../types/watch";
 
 const getAuthMiddleware = () => ({
   pre: async (context: RequestContext) => {
@@ -35,63 +25,3 @@ const apiConfig = new Configuration({
 });
 
 export const apiClient = new DefaultApi(apiConfig);
-
-export default class AppService {
-  static serieEventEmitter =
-    new EventEmitter() as TypedEmitter<IWatchEventEmitter>;
-  static fetchSeries = (
-    id: string,
-    season: number,
-    chapter: number
-  ): (() => void) => {
-    const response = new EventSource(
-      `${BASE_URL}/api/watch/?id=${id}&season_number=${season}&episode_number=${chapter}`
-    );
-
-    const eventHandler = (event: MessageEvent) => {
-      const data = JSON.parse(event.data) as IWatchEvent;
-
-      switch (data.type) {
-        case "init":
-          const initData = data as IWatchEventInit;
-          this.serieEventEmitter.emit("event", initData);
-          break;
-
-        case "trying_provider":
-          const tryingProviderData = data as IWatchEventTryingProvider;
-
-          this.serieEventEmitter.emit("event", tryingProviderData);
-          break;
-
-        case "provider_success":
-          const providerSuccessData = data as IWatchEventProviderSuccess;
-
-          this.serieEventEmitter.emit("event", providerSuccessData);
-          break;
-
-        case "provider_failed":
-          const providerFailedData = data as IWatchEventProviderFailed;
-
-          this.serieEventEmitter.emit("event", providerFailedData);
-          break;
-
-        case "end":
-          this.serieEventEmitter.emit("end");
-          break;
-      }
-    };
-
-    response.addEventListener("message", eventHandler);
-    response.addEventListener("error", (event) => {
-      console.error("Error:", event);
-    });
-    response.addEventListener("open", () => {
-      console.log("Open");
-    });
-
-    return () => {
-      response.close();
-      response.removeEventListener("message", eventHandler);
-    };
-  };
-}
