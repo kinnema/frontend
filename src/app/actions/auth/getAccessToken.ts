d416450597f1861d31328ffaa9ea2d2ca207d042
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2025-07-23 22:27:56 +0200

    fix: remove next.js actions

--- src/app/actions/auth/getAccessToken.ts
+++
@@ -1,5 +0,0 @@
-// Client-side version of getAccessToken
-export async function getAccessToken() {
-  // For client-side, we'll get the token from localStorage or make an API call
-  return localStorage.getItem('access_token');
-}
