4353582dad9e8469939cf64523299e819cfa9e9a
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2025-08-31 10:55:06 +0200

    feat: update P2P configuration to use environment variables for TURN server

--- src/lib/utils/p2p/config.ts
+++ src/lib/utils/p2p/config.ts
@@ -1,6 +1,12 @@
 import { BaseRoomConfig, RelayConfig, TurnConfig } from "trystero";
 
 const password = import.meta.env.VITE_P2P_KEY;
+const _TURN_SERVER =
+  (import.meta.env.VITE_TURN_SERVER as string)?.split("@") ?? [];
+const [credentials = "", server = ""] = _TURN_SERVER;
+const [TURN_USERNAME = "", TURN_CREDENTIAL = ""] = credentials.split(":") ?? [];
+const TURN_SERVER = server;
+
 type P2PConfig = BaseRoomConfig & RelayConfig & TurnConfig;
 
 export const getP2pConfig: (config?: Partial<P2PConfig>) => P2PConfig = (
@@ -17,9 +23,9 @@ export const getP2pConfig: (config?: Partial<P2PConfig>) => P2PConfig = (
       ],
     },
     {
-      urls: "turn:161.35.65.1:3478",
-      username: "username",
-      credential: "password",
+      urls: `turn:${TURN_SERVER}`,
+      username: TURN_USERNAME,
+      credential: TURN_CREDENTIAL,
     },
   ],
   ...config,
